94bd9355d791fcfb6a138db66a03dd325c747ea6
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2019-08-01 23:11:47 -0700

    Add full trust app service base classes

---
+++ Link10/Link10_UWP.FullTrust/AppServices/FullTrust/FullTrustAppServiceUWPEndPoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation.Metadata;
+
+namespace Link10.AppServices.FullTrust
+{
+    public abstract class FullTrustAppServiceUWPEndPoint : AppServiceConnectionEndPoint
+    {
+        protected FullTrustAppServiceUWPEndPoint(AppServiceConnection connection) 
+            : base(connection, true) // UWP always listen to active connection from Win32. 
+        {
+        }
+
+        public static async Task InvokeWin32EndpointAsync()
+        {
+            if (ApiInformation.IsApiContractPresent("Windows.ApplicationModel.FullTrustAppContract", 1, 0))
+            {
+                await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
+            }
+            else
+            {
+                throw new InvalidOperationException("FullTrustAppContract is not present.");
+            }
+        }
+    }
+}
---
+++ Link10/Link10_Win32/AppServices/FullTrust/FullTrustAppServiceWin32Endpoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Link10.AppServices;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.AppService;
+
+namespace Link10.AppServices.FullTrust
+{
+    public abstract class FullTrustAppServiceWin32Endpoint : AppServiceConnectionInitiator
+    {
+        public FullTrustAppServiceWin32Endpoint(string fullTrustAppServiceName)
+            : base (fullTrustAppServiceName, Package.Current.Id.FamilyName)
+        {
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+        }
+
+        protected virtual void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
