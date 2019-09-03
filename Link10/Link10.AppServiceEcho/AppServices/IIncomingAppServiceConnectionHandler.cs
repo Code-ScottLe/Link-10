2f8529ab8c1ba4cf268c913977d82c423b6b8ce6
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2019-09-03 00:45:01 -0700

    Add sample app containing echo service.

---
+++ Link10/Link10.AppServiceEcho/AppServices/IIncomingAppServiceConnectionHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace Link10.AppServiceEcho.AppServices
+{
+    public interface IIncomingAppServiceConnectionHandler
+    {
+        void OnIncomingAppServiceConnection(IBackgroundTaskInstance taskInstance);
+    }
+}
