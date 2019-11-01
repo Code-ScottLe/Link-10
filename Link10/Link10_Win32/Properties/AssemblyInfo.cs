faf6e52c8ebd4b65e7dc9f515b5ace3003f441ef
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2019-11-01 00:18:27 -0700

    Moving everybody to nuget

--- Link10/Link10_Win32/Properties/AssemblyInfo.cs
+++ Link10/Link10_Win32/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Link10_Win32")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Link10.Win32")]
+[assembly: AssemblyDescription("Link10 Library for Win32")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Link10_Win32")]
+[assembly: AssemblyCompany("Code.ScottLe")]
+[assembly: AssemblyProduct("Link10.Win32")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
