6fb2e966aaad08b4ebed2dfe80ef793975c431d3
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2020-06-13 03:08:31 -0700

    Added host and controller base.

--- Link10/Link10/AppServices/AppServiceHost.cs
+++ Link10/Link10/AppServices/AppServiceHost.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reactive.Subjects;
 using System.Text;
 
 using Windows.ApplicationModel.AppService;
@@ -12,8 +14,45 @@ namespace Link10.AppServices
     /// </summary>
     public class AppServiceHost
     {
+        private static AppServiceHost _instance;
+
+        public static AppServiceHost Instance
+        {
+            get
+            {
+                if (_instance != null)
+                {
+                    _instance = new AppServiceHost();
+                }
+
+                return _instance;
+            }
+        }
+
+        private Subject<AppServiceConnection> _connectionObservable;
+
+        public IObservable<AppServiceConnection> ConnectionObservable
+        {
+            get => _connectionObservable;
+        }
+
+        private AppServiceHost()
+        {
+            _connectionObservable = new Subject<AppServiceConnection>();
+        }
+
         public void HandleIncomingRequest(IBackgroundTaskInstance instance, AppServiceTriggerDetails triggerDetails)
         {
+            if(_connectionObservable.HasObservers)
+            {
+                _connectionObservable.OnNext(triggerDetails.AppServiceConnection);
+            }
+            else
+            {
+                // WARNING: Data lost. No observer/handler. Quit it.
+                Debug.WriteLine($"FATAL: Dropping connection inbound for {triggerDetails.Name} from pfn: {triggerDetails.CallerPackageFamilyName}.");
+                triggerDetails.AppServiceConnection.Dispose();
+            }
         }
     }
 
---
+++ Link10/Link10/AppServices/ControllerBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Link10.AppServices
+{
+    public class ControllerBase
+    {
+
+        public virtual async Task IndexAsync()
+        {
+
+        }
+    }
+}
