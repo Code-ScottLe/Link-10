e02116569554826d40ce9f421b75414d130e4e33
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2020-06-12 23:45:29 -0700

    Update Nuget and remove older stuffs.

---
+++ Link10/Link10/AppServices/AppServiceHost.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+
+namespace Link10.AppServices
+{
+    /// <summary>
+    /// Responsible for hosting/handling all incoming app services request.
+    /// </summary>
+    public class AppServiceHost
+    {
+        public void HandleIncomingRequest(IBackgroundTaskInstance instance, AppServiceTriggerDetails triggerDetails)
+        {
+        }
+    }
+
+}
